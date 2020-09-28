ee350b060051ce83add27955484b15fb637ff1d7
j11ridge <jmcass99@gmail.com>
2020-09-28 14:00:24 +0100

    switching back to master

---
+++ mysql_db.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DbDocjc
+{
+    public class mysql_db
+    {
+        private MySqlConnection conn { get; set; }
+
+        public bool hasError { get; private set; }
+        public string ErrorMsg { get; private set; }
+
+        public mysql_db()
+        {
+
+        }
+    }
+}
