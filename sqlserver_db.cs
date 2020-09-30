5182b751cc78d4db7b64ae1b796ce9bff9c6b4ce
j11ridge <jmcass99@gmail.com>
2020-09-30 09:07:58 +0100

    Started sql server

---
+++ sqlserver_db.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbDocjc
+{
+    public class sqlserver_db : _db
+    {
+    }
+}
